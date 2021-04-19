3524cc995ad22c873a276b6896f341b33b41ad67
joshuatydixon <joshtydixon@gmail.com>
2021-04-19 10:34:13 -0400

    about page html

---
+++ src/views/About.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        
+        <div>
+            
+
+    <div>
+          <h1>About us </h1>
+        </div>
+          
+        <div>
+            <img src="https://www.startupguys.net/wp-content/uploads/2018/12/lead-software-development-team.jpg" width= "600" height="300" alt="" className="img-fluid"/>
+        </div>
+    
+    
+  
+<div>
+          <h2>Our mission is to get you on the right track</h2>
+          <p>If you are often overwhelmed by the amount of tasks, objectives and goals you have to complete, deadlines or other important items that you need to remember, a to-do list can help you put it all together. It's one of the best tools you can use. It'll help keep your management tasks organized and on track.</p>
+          </div>
+        
+
+          <h1>Our team</h1>
+  <div>
+          <img src="https://cdn1-www.superherohype.com/assets/uploads/2013/11/batmane3-1.jpg" alt="" width="300" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+            <h3 className="mb-0">Bats</h3>
+            <span>Title/position</span>
+            <ul >
+              <li>Facebook</li>
+              <li>Instagram</li>
+              <li>LinkedIn</li>
+              <li>Github</li>
+            </ul>
+          </div>
+
+          <div>
+            <img src="https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fblogs-images.forbes.com%2Fjvchamary%2Ffiles%2F2016%2F03%2Fman_of_steel-1200x800.jpg" alt="" width="300"/>
+              <h3>Supes</h3>
+              <span>Title/Position</span>
+              <ul >
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>LinkedIn</li>
+                <li>Github</li>
+              </ul>
+            </div>
+
+            <div>
+                <img src="https://www.previewsworld.com/SiteImage/MainImage/STL163590.jpg" alt="" width="300"/>
+                  <h3>Storm</h3>
+                  <span>Title/Position</span>
+                  <ul >
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>LinkedIn</li>
+                    <li>Github</li>
+                  </ul>
+                </div>
+
+                <div>
+                    <img src="https://live.staticflickr.com/65535/51120182125_719d6c011c_b.jpg" alt="" width="300"/>
+                      <h3>Joshua Dixon</h3>
+                      <span>Title/Position</span>
+                      <ul >
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                        <li>LinkedIn</li>
+                        <li>Github</li>
+                      </ul>
+                    </div>
+        
+
+
+  
+  
+  <footer>
+    <div>
+      <p>&copy; Copyrights ToDoList.com All rights reserved.</p>
+    </div>
+  </footer>
+        </div>
+       
+        
+    )
+}
